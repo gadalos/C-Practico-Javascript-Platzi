e0d336e6f28ed3b95365b3152f3e59dbf9cafa16
DARWIN CATUNTA <83663953+gadalos@users.noreply.github.com>
2021-07-08 19:47:33 -0500

    Introduciendo funciones para haller precioConDescuento

--- taller-2/descuentos.js
+++ taller-2/descuentos.js
@@ -1,16 +1,25 @@
-const precioOriginal = 120;
-const descuento = 15;
+// const precioOriginal = 120;
+// const descuento = 18;
 
 // Versión Platzi
 // const porcentajePrecioConDescuento = 100 - descuento;
 // const precioConDescuento = (precioOriginal * porcentajePrecioConDescuento)/100;
+
+
+// Versión Personal
+// const precioDescuento = precioOriginal * (descuento)/100;
+// const precioFinal = precioOriginal - precioDescuento;
+
+// console.log("El precio que se debe pagar es: " + precioFinal + " soles");
+
 // console.log({
 //     precioOriginal, descuento, precioDescuento, precioFinal
 // });
 
-// Versión Personal
-const precioDescuento = precioOriginal * (descuento)/100;
-const precioFinal = precioOriginal - precioDescuento;
+function calcularPrecioConDescuento(precio, descuento) {
+    const precioDescuento = precio * (descuento)/100;
+    const precioFinal = precio - precioDescuento;
 
-console.log("El precio que se debe pagar es: " + precioFinal + " soles");
+    return precioFinal;
+}
 
