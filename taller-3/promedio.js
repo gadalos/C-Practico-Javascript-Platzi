d03df0d07103c669e503c88ef2150c01b04e52ee
DARWIN CATUNTA <83663953+gadalos@users.noreply.github.com>
2021-07-10 13:29:24 -0500

    Comenzando el taller 3 con promedio

---
+++ taller-3/promedio.js
@@ -0,0 +1,11 @@
+const lista1 = [100, 200, 300, 500];
+
+let sumaLista = 0;
+
+for (let i = 0; i < lista1.length; i++) {
+  sumaLista = sumaLista + lista1[i];
+}
+const promedioLista = sumaLista / lista1.length;
+
+//  Para haller el promedio se suma todas las variables y se divide por el total de variables sumadas
+// promedio = (h1 + h2 + ... + hn)/ pn
