89af8391fa28c27d232ff4077302747d0b17c27b
DARWIN CATUNTA <83663953+gadalos@users.noreply.github.com>
2021-07-10 13:42:45 -0500

    Funcion calcularMediaAritmética con el ciclo for

--- taller-3/promedio.js
+++ taller-3/promedio.js
@@ -1,11 +1,16 @@
-const lista1 = [100, 200, 300, 500];
+// const lista = [100, 200, 300, 500];
 
-let sumaLista = 0;
-
-for (let i = 0; i < lista1.length; i++) {
-  sumaLista = sumaLista + lista1[i];
-}
-const promedioLista = sumaLista / lista1.length;
 
 //  Para haller el promedio se suma todas las variables y se divide por el total de variables sumadas
 // promedio = (h1 + h2 + ... + hn)/ pn
+
+function calcularMediaAritmetica(lista) {
+    let sumaLista = 0;
+
+    for (let i = 0; i < lista.length; i++) {
+    sumaLista = sumaLista + lista[i];
+    }
+    const promedioLista = sumaLista / lista.length;
+    
+    return promedioLista;
+}
