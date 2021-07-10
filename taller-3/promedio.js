fa28aec8b237ec01d41124f0d5437810e0c9efc4
DARWIN CATUNTA <83663953+gadalos@users.noreply.github.com>
2021-07-10 14:24:44 -0500

    Calcular la media Aritmetica con el metodo del array ' reduce() '

--- taller-3/promedio.js
+++ taller-3/promedio.js
@@ -1,16 +1,22 @@
 // const lista = [100, 200, 300, 500];
 
-
 //  Para haller el promedio se suma todas las variables y se divide por el total de variables sumadas
 // promedio = (h1 + h2 + ... + hn)/ pn
 
 function calcularMediaAritmetica(lista) {
-    let sumaLista = 0;
+  // let sumaLista = 0;
 
-    for (let i = 0; i < lista.length; i++) {
-    sumaLista = sumaLista + lista[i];
-    }
-    const promedioLista = sumaLista / lista.length;
-    
-    return promedioLista;
+  // for (let i = 0; i < lista.length; i++) {
+  // sumaLista = sumaLista + lista[i];
+  // }
+
+  const sumaLista = lista.reduce(function (valorAcumulado = 0, nuevoElemento) {
+    // console.log(valorAcumulado);
+    // console.log(nuevoElemento);
+    return valorAcumulado + nuevoElemento;
+  });
+
+  const promedioLista = sumaLista / lista.length;
+
+  return promedioLista;
 }
