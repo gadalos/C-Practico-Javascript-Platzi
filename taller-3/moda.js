553a9590f7cbedd8e051835deb4436c70c55e7d9
Darwin Catunta <83663953+gadalos@users.noreply.github.com>
2021-07-18 20:01:23 -0500

    Se obtuvo el calculo de la moda mediante la consola

--- taller-3/moda.js
+++ taller-3/moda.js
@@ -2,20 +2,29 @@ const lista = [10, 20, 30, 10, 20, 30, 40, 50, 10];
 
 const listaContador = {};
 
-lista.map(
-    function (elemento) {
-        if (listaContador[elemento]) {
-            // listaContador[elemento] = listaContador[elemento] + 1;
-            listaContador[elemento] += 1;
-        } else{
-            listaContador[elemento] = 1;
-        }
-        
+lista.map(function (elemento) {
+  if (listaContador[elemento]) {
+    // listaContador[elemento] = listaContador[elemento] + 1;
+    listaContador[elemento] += 1;
+  } else {
+    listaContador[elemento] = 1;
+  }
+});
+
+const listaArray = Object.entries(listaContador).sort(
+    function (elementoA, elementoB) {
+        return elementoA[1] - elementoB[1];
     }
 );
 
+const moda = listaArray[listaArray.length - 1];
+
 function calcularModa() {
-    entradaModa  = document.getElementById("inputModa");
-    modaValue = entradaModa.value;
+  entradaModa = document.getElementById("inputModa");
+  modaValue = entradaModa.value;
+
+  let arrayValor = Array.from(modaValue.split(","), Number);
+  
+  
 
 }
