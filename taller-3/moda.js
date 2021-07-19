51518813cd140f82f002c6c640316251ce2f2349
Darwin Catunta <83663953+gadalos@users.noreply.github.com>
2021-07-18 20:02:13 -0500

    Se incluyo una funcion calcularModa() para el calculo de la moda y respuesta al html

--- taller-3/moda.js
+++ taller-3/moda.js
@@ -1,30 +1,28 @@
-const lista = [10, 20, 30, 10, 20, 30, 40, 50, 10];
-
-const listaContador = {};
-
-lista.map(function (elemento) {
-  if (listaContador[elemento]) {
-    // listaContador[elemento] = listaContador[elemento] + 1;
-    listaContador[elemento] += 1;
-  } else {
-    listaContador[elemento] = 1;
-  }
-});
-
-const listaArray = Object.entries(listaContador).sort(
-    function (elementoA, elementoB) {
-        return elementoA[1] - elementoB[1];
-    }
-);
-
-const moda = listaArray[listaArray.length - 1];
+// const lista = [10, 20, 30, 10, 20, 30, 40, 50, 10];
 
 function calcularModa() {
   entradaModa = document.getElementById("inputModa");
   modaValue = entradaModa.value;
 
   let arrayValor = Array.from(modaValue.split(","), Number);
-  
-  
 
+  const listaContador = {};
+
+  arrayValor.map(function (elemento) {
+    if (listaContador[elemento]) {
+      // listaContador[elemento] = listaContador[elemento] + 1;
+      listaContador[elemento] += 1;
+    } else {
+      listaContador[elemento] = 1;
+    }
+  });
+
+  const listaArray = Object.entries(listaContador).sort(function (elementoA, elementoB) {
+    return elementoA[1] - elementoB[1];
+  });
+
+  const moda = listaArray[listaArray.length - 1];
+  
+  const mostrarResultado = document.getElementById("resultadoModa");
+  mostrarResultado.innerText = "La moda es de: " + moda;
 }
